66bb202f8eea354e9d4a6d970522b78076336889
vermarish <rishabhverma.rv@gmail.com>
2021-03-22 21:38:36 -0700

    Add comments to main.py loop

--- main.py
+++ main.py
@@ -31,8 +31,8 @@ base_address = '/'.join(['http:/',
 # a dictionary containing each light's address from which to query
 address = {ID: '/'.join([base_address, str(ID)]) for ID in active_IDs}
 
-
-## Store the vertices of each light's color gamut
+## For rainbow()
+# Store the vertices of each light's color gamut for rainbow()
 gamut_vertices = {}
 for ID in active_IDs:
 	gamut_vertices[ID] = requests.get(address[ID]).json()['capabilities']['control']['colorgamut']
@@ -56,31 +56,42 @@ def rainbow(IDs, time_per_cycle=2, cycles=1):
 
 
 league_address = "​https://127.0.0.1:2999/liveclientdata/allgamedata/"
+
+## Main loop
 while True:
 	try:
 		response = requests.get(league_address, verify=False)
 	except:
+		# Game's not running yet.
 		response = None
 		initialized = False
 		next_event_id = 0
 		sleep(1)
 
-	if response is not None:  # then we're live!
+	if response is not None:  # Game's running.
 		json = response.json()
-		if not initialized:  # let's store the summoner's info and their skin
+
+		# If we've just started,
+		if not initialized:  # Let's store the summoner's info and their skin
 			summonerName = json["activePlayer"]["summonerName"]
 			playerIndex = 0
 			while json["allPlayers"][playerIndex]["summonerName"] != summonerName:
 				playerIndex += 1
 			if playerIndex >= 10:
+				# TODO can i just delete this if-block?
 				print("Somehow, this game has eleven players..")
 				print("or none of the players match the current user.")
 				break
 
-			# Now we have their playerIndex, and can get their playerInfo!
+			# Now we have their playerIndex. Let's get their playerInfo!
 			playerInfo = json["allPlayers"][playerIndex]
 			championName = playerInfo[championName]
-			formattedName = "".join(championName.split())  # Master Yi's URL is .../MasterYi.json
+
+			# TODO there exists no consistent function to get a champions "[id].json". 
+			#      See Kog'Maw -> KogMaw.json, Cho'Gath -> Chogath.json.
+			#      Gonna have to find a mapping somewhere from 
+			formattedName = "".join(championName.split()) 
+
 			skinID = playerInfo["skinID"]
 			team = playerInfo["team"]
 
