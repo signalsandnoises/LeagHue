e128b928100549e74cb1b36389c66fc131d4fd12
Rishabh Verma <rishabhverma.rv@gmail.com>
2022-04-05 15:51:30 -0700

    setup.py can now find and configure lights.

--- setup.py
+++ setup.py
@@ -18,8 +18,12 @@ def connectToBridge():
 
     # Search for bridges
     print("Searching for bridges.. ", end="")
-    res = requests.get("https://discovery.meethue.com")
-    if res.status_code != 200: throwError("ERROR: Unable to query discovery.meethue.com to find your bridge. Check your internet connection.")
+    try:
+        res = requests.get("https://discovery.meethue.com")
+        if res.status_code != 200:
+            raise Exception
+    except Exception as e:
+        throwError("ERROR: Unable to query discovery.meethue.com to find your bridge. Check your internet connection.")
     bridges = res.json()
 
     # Figure out which bridge to configure with
@@ -77,19 +81,64 @@ def authenticateBridge(address, bridge_id):
         db.insert_bridge_key(bridge_id, user, key)
 
 
-    print("Storing key.. ", end="")
     config["Philips Hue"]['User'] = user
     config["Philips Hue"]['Key'] = key
-    print("Done.")
+    print("Bridge key found and configured.")
     return (user, key)
 
 def selectLights(address, user, key):
-    # TODO this whole thang.
-    pass
+    request_header = {"hue-application-key": user}
+    request_url = f"https://{address}/clip/v2/resource/light"
+    try:
+        res = requests.get(url=request_url, headers=request_header,
+                           verify=False)
+                           #cert="cert.pem")  # TODO figure out SSL and clean this try/except
+        if res.status_code != 200:
+            raise BaseException
+    except:
+        throwError("ERROR: Unable to get lights from Bridge.")
+    lights = res.json()
+    num_lights = len(lights['data'])
+    light_names = [lights['data'][i]['metadata']['name'] for i in range(num_lights)]
+    for i, light_name in enumerate(light_names):
+        print(f"[{i}]: {light_name}")
+
+    lights_to_add = set()
+    print(res.json())
+    print("For each light you wish to include, enter its index.")
+    print("If you add a light on accident, re-enter its index to remove it.")
+    print("When the right lights have been selected, enter 'next'")
+    next = False
+    while not next:
+        line = input(f"Currently selected: {lights_to_add}\n>> ")
+        next = line == "next"
+        if not next:
+            try:
+                line = int(line)
+                if line >= num_lights or line < 0: raise ValueError
+                if line not in lights_to_add:
+                    lights_to_add.add(line)
+                else:
+                    lights_to_add.remove(line)
+            except ValueError:
+                print("ERROR: Invalid light index. Try a valid number.")
+
+        if next:
+            print(f"Are you sure you wish to continue with the following lights? [Y/n]")
+            for i in lights_to_add:
+                print(f"[{i}]: {light_names[i]}")
+            line = input(">> ")
+            if line == "n":
+                next = False
+    config["Philips Hue"]["lights"] = ",".join([str(l) for l in lights_to_add])
+    print("Adding these lights.")
 
 
-def setup():
-    proceed = input("Welcome to the LeagHue configuration wizard! Would you like to configure LeagHue? [y/N]\n>>")
+def setup(debug=False):
+    proceed = "y"
+    if not debug:
+        proceed = input("Welcome to the LeagHue configuration wizard! Would you like to configure LeagHue?\n"
+                    "(Your existing configuration will be overwritten) [y/N]\n>>")
     if proceed != "y":
         throwError("Cancelling configuration.")
 
@@ -97,9 +146,9 @@ def setup():
     address, bridge_id = connectToBridge()  # configuration stuff
     print("STEP ONE COMPLETE.")
 
-    print("\nSTEP TWO: Authenticate and Connect to the Hue Bridge ")
+    print("\nSTEP TWO: Authenticate and Connect to the Hue Bridge")
     user, key = authenticateBridge(address, bridge_id)  # authentication stuff
-    print("STEP TWO COMPMLETE.")
+    print("STEP TWO COMPLETE.")
 
     print("\nSTEP THREE: Select Lights")
     selectLights(address, user, key)  # configuration stuff
@@ -108,7 +157,9 @@ def setup():
     with open('config.ini', 'w') as configfile:
         config.write(configfile)
 
+    print("CONFIGURATION COMPLETE.")
+
 
 if __name__ == '__main__':
     config = configparser.ConfigParser()
-    setup()
+    setup(debug=True)  # TODO change this to False
